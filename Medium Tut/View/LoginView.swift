ae448051031e2eebf5afdde51e1ba74c30b323d0
Deonte Kilgore <deontecodes@gmail.com>
2019-09-19 15:36:12 -0400

    Finished.

--- Medium Tut/View/LoginView.swift
+++ Medium Tut/View/LoginView.swift
@@ -12,62 +12,147 @@ class LoginView: UIView {
     
     let heroView: UIView = {
         let view = UIView()
-        view.backgroundColor = .red
+        view.frame.origin.y -= 450
+        view.backgroundColor = .heroMain
+        view.layer.cornerRadius = 36
+        view.layer.maskedCorners = [.layerMinXMaxYCorner,.layerMaxXMaxYCorner]
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
     
     let heroImage: UIImageView = {
         let imageView = UIImageView()
-        imageView.backgroundColor = .yellow
+        imageView.image = Images.illustration
         imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
     
     let titleLabel: UILabel = {
         let label = UILabel()
-        label.backgroundColor = .purple
+        label.text = "Learn design & code.\nFrom scratch."
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 34, weight: .bold)
+        label.numberOfLines = 0
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
     let subtitleLabel: UILabel = {
         let label = UILabel()
-        label.backgroundColor = .blue
+        label.numberOfLines = 0
+        label.text = "70 hours of video courses teaching SwiftUI,\nReact and design tools."
+        label.font = UIFont.systemFont(ofSize: 17, weight: .regular)
+        label.textAlignment = .center
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
     let bottomView: UIView = {
         let view = UIView()
-        view.backgroundColor = .green
+        view.alpha = 0
+        view.frame.origin.y += 0
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
     
     let inputContainerView: UIView = {
         let view = UIView()
-        view.backgroundColor = .black
+        view.layer.cornerRadius = 20
+        view.clipsToBounds = true
+        view.backgroundColor = .white
+        view.layer.masksToBounds = false
+        view.layer.applySketchShadow(color: .shadowDark, alpha: 0.1, x: 0, y: 40.0, blur: 40.0, spread: 0)
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
     
+    let inceptionContainerView: UIView = {
+        let view = UIView()
+        view.layer.cornerRadius = 20
+        view.clipsToBounds = true
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    let line: UIView = {
+        let view = UIView()
+        view.backgroundColor = .lineColor
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    let emailTextContainer: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    let passwordTextContainer: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    
     let forgotPasswordButton: UIButton = {
         let button = UIButton(type: .system)
-        button.backgroundColor = .red
+        button.setTitle("Forgot password?", for: .normal)
+        button.setTitleColor(.textColor, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 15, weight: .medium)
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
     
     let loginButton: UIButton = {
         let button = UIButton(type: .system)
-        button.backgroundColor = .red
+        button.setTitle("LOG IN", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 15, weight: .semibold)
+        button.titleLabel?.textAlignment = .center
+        button.backgroundColor = .buttonBackground
+        button.tintColor = .white
+        button.layer.cornerRadius = 16
+        button.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMinYCorner, .layerMaxXMaxYCorner]
+        button.layer.applySketchShadow(color: .buttonBackground, alpha: 0.3, x: 0, y: 10.0, blur: 20.0, spread: 0)
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
     
+    let emailImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = Images.email
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    let passwordImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = Images.password
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    let emailTextField: UITextField = {
+        let tf = UITextField()
+        tf.attributedPlaceholder = NSAttributedString(string: "YOUR EMAIL",
+                                                      attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 15, weight: .medium),
+                                                                   NSAttributedString.Key.foregroundColor : UIColor.textColor])
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        return tf
+    }()
+    
+    let passwordTextField: UITextField = {
+        let tf = UITextField()
+        tf.attributedPlaceholder = NSAttributedString(string: "PASSWORD",
+                                                      attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 15, weight: .medium),
+                                                                   NSAttributedString.Key.foregroundColor : UIColor.textColor])
+        tf.isSecureTextEntry = true
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        return tf
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setupAnimations()
         setupView()
     }
     
@@ -79,6 +164,17 @@ class LoginView: UIView {
         backgroundColor = .background
         constrainTop()
         constrainBottom()
+    }
+    
+    fileprivate func setupAnimations() {
+        UIView.animate(withDuration: 0.8, delay: 0, usingSpringWithDamping: 0.9, initialSpringVelocity: 0.4, options: [.curveEaseInOut], animations: {
+            self.heroView.frame.origin.y += 400
+        })
+        
+        UIView.animate(withDuration: 1, delay: 0.1, options:[.curveEaseInOut], animations: {
+            self.bottomView.alpha = 1
+            self.bottomView.frame.origin.y -= 10
+        })
     }
     
     fileprivate func constrainTop() {
@@ -125,7 +221,63 @@ class LoginView: UIView {
         addConstraint(NSLayoutConstraint(item: inputContainerView, attribute: .trailing, relatedBy: .equal, toItem: bottomView, attribute: .trailing, multiplier: 1, constant: -16))
         addConstraint(NSLayoutConstraint(item: inputContainerView, attribute: .leading, relatedBy: .equal, toItem: bottomView, attribute: .leading, multiplier: 1, constant: 16))
         addConstraint(NSLayoutConstraint(item: inputContainerView, attribute: .height, relatedBy: .equal, toItem: bottomView.self, attribute: .height, multiplier: 0.55, constant: 0))
+
+        inputContainerView.addSubview(inceptionContainerView)
         
+        addConstraint(NSLayoutConstraint(item: inceptionContainerView, attribute: .top, relatedBy: .equal, toItem: inputContainerView, attribute: .top, multiplier: 1, constant: 0))
+        addConstraint(NSLayoutConstraint(item: inceptionContainerView, attribute: .trailing, relatedBy: .equal, toItem: inputContainerView, attribute: .trailing, multiplier: 1, constant: 0))
+        addConstraint(NSLayoutConstraint(item: inceptionContainerView, attribute: .leading, relatedBy: .equal, toItem: inputContainerView, attribute: .leading, multiplier: 1, constant: 0))
+        addConstraint(NSLayoutConstraint(item: inceptionContainerView, attribute: .bottom, relatedBy: .equal, toItem: inputContainerView, attribute: .bottom, multiplier: 1, constant: 0))
+      
+        inceptionContainerView.addSubview(emailTextContainer)
+        
+        addConstraint(NSLayoutConstraint(item: emailTextContainer, attribute: .top, relatedBy: .equal, toItem: inputContainerView, attribute: .top, multiplier: 1, constant: 0))
+        addConstraint(NSLayoutConstraint(item: emailTextContainer, attribute: .trailing, relatedBy: .equal, toItem: inputContainerView, attribute: .trailing, multiplier: 1, constant: 0))
+        addConstraint(NSLayoutConstraint(item: emailTextContainer, attribute: .leading, relatedBy: .equal, toItem: inputContainerView, attribute: .leading, multiplier: 1, constant: 0))
+        addConstraint(NSLayoutConstraint(item: emailTextContainer, attribute: .height, relatedBy: .equal, toItem: inputContainerView.self, attribute: .height, multiplier: 0.5, constant: 0))
+        
+        inceptionContainerView.addSubview(passwordTextContainer)
+        
+        addConstraint(NSLayoutConstraint(item: passwordTextContainer, attribute: .bottom, relatedBy: .equal, toItem: inputContainerView, attribute: .bottom, multiplier: 1, constant: 0))
+        addConstraint(NSLayoutConstraint(item: passwordTextContainer, attribute: .trailing, relatedBy: .equal, toItem: inputContainerView, attribute: .trailing, multiplier: 1, constant: 0))
+        addConstraint(NSLayoutConstraint(item: passwordTextContainer, attribute: .leading, relatedBy: .equal, toItem: inputContainerView, attribute: .leading, multiplier: 1, constant: 0))
+        addConstraint(NSLayoutConstraint(item: passwordTextContainer, attribute: .height, relatedBy: .equal, toItem: inputContainerView.self, attribute: .height, multiplier: 0.5, constant: 0))
+        
+        emailTextContainer.addSubview(emailImage)
+        
+        addConstraint(NSLayoutConstraint(item: emailImage, attribute: .centerY, relatedBy: .equal, toItem: emailTextContainer, attribute: .centerY, multiplier: 1, constant: 0))
+        addConstraint(NSLayoutConstraint(item: emailImage, attribute: .leading, relatedBy: .equal, toItem: emailTextContainer, attribute: .leading, multiplier: 1, constant: 12))
+        addConstraint(NSLayoutConstraint(item: emailImage, attribute: .width, relatedBy: .equal, toItem: self, attribute: .width, multiplier: 0, constant: 44))
+        addConstraint(NSLayoutConstraint(item: emailImage, attribute: .height, relatedBy: .equal, toItem: self, attribute: .height, multiplier: 0, constant: 44))
+        
+        emailTextContainer.addSubview(emailTextField)
+        
+        addConstraint(NSLayoutConstraint(item: emailTextField, attribute: .top, relatedBy: .equal, toItem: emailTextContainer, attribute: .top, multiplier: 1, constant: 0))
+        addConstraint(NSLayoutConstraint(item: emailTextField, attribute: .leading, relatedBy: .equal, toItem: emailImage, attribute: .trailing, multiplier: 1, constant: 26))
+        addConstraint(NSLayoutConstraint(item: emailTextField, attribute: .trailing, relatedBy: .equal, toItem: emailTextContainer, attribute: .trailing, multiplier: 1, constant: 0))
+        addConstraint(NSLayoutConstraint(item: emailTextField, attribute: .bottom, relatedBy: .equal, toItem: emailTextContainer, attribute: .bottom, multiplier: 1, constant: 0))
+        
+        passwordTextContainer.addSubview(passwordImage)
+        
+        addConstraint(NSLayoutConstraint(item: passwordImage, attribute: .centerY, relatedBy: .equal, toItem: passwordTextContainer, attribute: .centerY, multiplier: 1, constant: 0))
+        addConstraint(NSLayoutConstraint(item: passwordImage, attribute: .leading, relatedBy: .equal, toItem: passwordTextContainer, attribute: .leading, multiplier: 1, constant: 12))
+        addConstraint(NSLayoutConstraint(item: passwordImage, attribute: .width, relatedBy: .equal, toItem: self, attribute: .width, multiplier: 0, constant: 44))
+        addConstraint(NSLayoutConstraint(item: passwordImage, attribute: .height, relatedBy: .equal, toItem: self, attribute: .height, multiplier: 0, constant: 44))
+        
+        passwordTextContainer.addSubview(passwordTextField)
+        
+        addConstraint(NSLayoutConstraint(item: passwordTextField, attribute: .top, relatedBy: .equal, toItem: passwordTextContainer, attribute: .top, multiplier: 1, constant: 0))
+        addConstraint(NSLayoutConstraint(item: passwordTextField, attribute: .leading, relatedBy: .equal, toItem: passwordImage, attribute: .trailing, multiplier: 1, constant: 26))
+        addConstraint(NSLayoutConstraint(item: passwordTextField, attribute: .trailing, relatedBy: .equal, toItem: passwordTextContainer, attribute: .trailing, multiplier: 1, constant: 0))
+        addConstraint(NSLayoutConstraint(item: passwordTextField, attribute: .bottom, relatedBy: .equal, toItem: passwordTextContainer, attribute: .bottom, multiplier: 1, constant: 0))
+       
+        inputContainerView.addSubview(line)
+        
+        addConstraint(NSLayoutConstraint(item: line, attribute: .centerY, relatedBy: .equal, toItem: inputContainerView, attribute: .centerY, multiplier: 1, constant: 0))
+        addConstraint(NSLayoutConstraint(item: line, attribute: .trailing, relatedBy: .equal, toItem: inputContainerView, attribute: .trailing, multiplier: 1, constant: 0))
+        addConstraint(NSLayoutConstraint(item: line, attribute: .leading, relatedBy: .equal, toItem: emailTextField, attribute: .leading, multiplier: 1, constant: 0))
+        addConstraint(NSLayoutConstraint(item: line, attribute: .height, relatedBy: .equal, toItem: inputContainerView.self, attribute: .height, multiplier: 0, constant: 2))
+     
         bottomView.addSubview(forgotPasswordButton)
         
         addConstraint(NSLayoutConstraint(item: forgotPasswordButton, attribute: .top, relatedBy: .equal, toItem: inputContainerView, attribute: .bottom, multiplier: 1, constant: 13))
